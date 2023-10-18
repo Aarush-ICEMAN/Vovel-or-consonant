cb5b37ec448a4bc39d7ae078e4638425ddacf216
Aarush-ICEMAN <146875714+Aarush-ICEMAN@users.noreply.github.com>
2023-10-18 21:24:12 +0530

    Add files via upload
    Vovel or consonant

---
+++ Vovel or Consonant.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	char A;
+	printf("Enter the Alphabet\n ");
+	scanf("%c",&A);
+	if (A=='A'||A=='E',A=='I'||A=='O'||A=='U'||A=='a'||A=='e'||A=='i'||A=='o'||A=='u'){
+		printf("The Alphabet entered is a Vovel",A);
+		
+	}
+	else {
+		printf("The Alphabet entered is a Consonant ",A);
+		
+	}
+	return 0;
+	system ("pause");
+	
+	
+	
+}
